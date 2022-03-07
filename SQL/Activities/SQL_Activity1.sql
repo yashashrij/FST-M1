8d8c56c04ac4f8e462a0310da2bb04966dbf5e05
yashashrij <67087861+yashashrij@users.noreply.github.com>
2022-03-07 12:13:18 +0530

    Add files via upload

---
+++ SQL/Activities/SQL_Activity1.sql
@@ -0,0 +1,24 @@
+REM   Script: Activity 1
+REM   Activity1 Completed
+
+select * from scott.emp;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
---
+++ SQL/Activities/SQL_Activity4.sql
@@ -0,0 +1,12 @@
+REM   Script: Activity4
+REM   Activity 4 Completed
+
+-- Add the grade column
+ALTER TABLE salesman ADD grade int;
+
+-- Update the values in the grade column
+UPDATE salesman SET grade=100;
+
+-- Display data
+SELECT * FROM salesman;;
+
